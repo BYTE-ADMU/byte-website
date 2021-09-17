b0d2f18887c64cb0b55359c39b0f25d5b975d0ed
M Jim Lee <mjimlee@gmail.com>
2021-09-17 15:31:34 +0800

    added partners display and minor banner edits

--- src/pages/partners.js
+++ src/pages/partners.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Layout from "../layouts/default"
+import partnersbg from "../assets/partners/partners-coming-soon.png"
 
 const partners = () => {
     return (
         <Layout>
 
-        <div>
-            <h1>Parters Page</h1>
+        <div
+            className="flex flex-col justify-center items-center content-center w-auto h-screen bg-no-repeat bg-top bg-contain px-10 sm:px-0" 
+            style={{
+            backgroundImage: `url(${partnersbg})`
+            }}> 
         </div>
         </Layout>
     )
